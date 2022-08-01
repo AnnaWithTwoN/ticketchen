71a68a8f63e8e9ed7cfbb405a9765b07868c1216
AnnaSidorova <anna.sidorova@equaleyes.com>
2022-08-01 22:09:51 +0200

    init

---
+++ src/main/java/com/annawithtwon/ticketchen/TicketchenApplication.java
@@ -0,0 +1,13 @@
+package com.annawithtwon.ticketchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketchenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/annawithtwon/ticketchen/TicketchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annawithtwon.ticketchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
